8e6eac7db4e620303b7c5f9bc73ec228921740f4
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-08-06 16:26:02 +0300

    Update storage.js

--- models/storage.js
+++ models/storage.js
@@ -1,9 +1,9 @@
 const fs = require('fs/promises');
-const uniqueid = require('uniqid')
+const uniqid = require('uniqid');
 
 let data = {};
 
-async function init() {
+async function init(){
     try {
         data = JSON.parse(await fs.readFile('./models/data.json'))
     } catch (err){
@@ -12,52 +12,69 @@ async function init() {
     
     return (req, res, next) => {
         req.storage = {
+            getById,
             getAll,
-            getById, 
-            create
-        }
+            create,
+            edit
+        };
         next();
-    }
+    };
 }
 
 async function getAll(query){
-    let cubes = Object
-      .entries(data)
-      .map(([id, v]) => Object.assign({}, { id }, v))
+    let cubes =  Object
+        .entries(data)
+        .map(([id, v]) => Object.assign({}, {id}, v)
+    )
 
-        if (query.search){
-          cubes = cubes.filter(c => c.name.toLowerCase().includes(query.search.toLowerCase()))
-        }
+    if (query.search){
+        cubes = cubes.filter(c => c.name.toLowerCase().includes(query.search.toLowerCase()));
+    }
+    if (query.from){
+        cubes = cubes.filter(c => c.difficulty >= Number(query.from));
+    }
+    if (query.to){
+        cubes = cubes.filter(c => c.difficulty <= Number(query.to));
+    }
 
-        if (query.from){
-            cubes = cubes.filter(c => c.difficulty >= Number(query.from))
-        }
-
-        if (query.to){
-            cubes = cubes.filter(c => c.difficulty <= Number(query.to))
-        }
     return cubes;
 }
 
 async function getById(id){
     const cube = data[id];
-    if (cube) {
-        return Object.assign({}, { id }, cube)
+
+    if (cube){
+        return Object.assign({}, {id}, cube)
     } else {
         return undefined;
     }
 }
 
 async function create(cube){
-    const id = uniqueid();
+    const id = uniqid()
     data[id] = cube;
 
+    await persist()
+}
+
+async function edit(id, cube){
+    if (!data[id]){
+        throw new ReferenceError('No such ID in database')
+    }
+    
+    data[id] = cube;
+
+    await persist();
+}
+
+async function persist(){
     try {
-        await fs.writeFile('./models/data.json', JSON.stringify(data, null, 2))
+        fs.writeFile('./models/data.json', JSON.stringify(data, null, 2))
         console.log('>>> created new record')
-    } catch(err){
+    } catch (err){
         console.error('Error writing out database')
-    }   
+    }
+    
 }
 
 module.exports = {
