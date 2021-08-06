71e92086fb071d74ecef9d66807d63b547c30e40
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-08-06 16:24:44 +0300

    Update edit.js

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,19 +1,27 @@
 module.exports = {
-    async edit (req, res) {
-        const cube = await req.storage.getById(req.params.id);
+    edit: async (req, res) => {
+        const id = req.params.id;
+        const cube = await req.storage.getById(id);
+        cube[`select${cube.difficulty}`] = true;
 
         if (!cube){
-            res.redirect('404')
+            res.redirect('/404')
         } else {
-            const ctx = {
-                title: 'Edit Cube',
-                cube
-            }
-            res.render('edit', ctx)
+            res.render('edit', cube)
         }
-        
     },
-    post (req, res) {
-        res.redirect('/')
+    editPost: async (req, res) => {
+        const cube = {
+            name: req.body.name,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+            difficulty: Number(req.body.difficulty)
+        };
+        try {
+            await req.storage.edit(req.params.id, cube);
+            res.redirect('/')
+        } catch(err){
+            res.redirect('/404')
+        }
     }
 }
